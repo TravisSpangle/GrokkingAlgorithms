1c2c70eb2c032b016b66eaa0fe0122dc05241b22
Travis Spangle <travis.spangle@k2sports.com>
2016-09-08 08:15:41 -0700

    do not round memory usuage

--- Travis/lib/wrapper.rb
+++ Travis/lib/wrapper.rb
@@ -20,7 +20,8 @@ def measure(&block)
     GC.start
   end
 
-  memory_before = `ps -o rss= -p #{Process.pid}`.to_i/1024
+  #memory_before = `ps -o rss= -p #{Process.pid}`.to_i/512
+  memory_before = `ps -o rss= -p #{Process.pid}`.to_i
   gc_stat_before = GC.stat
   time = Benchmark.realtime do
     yield
@@ -31,14 +32,14 @@ def measure(&block)
   end
 #  puts ObjectSpace.count_objects
   gc_stat_after = GC.stat
-  memory_after = `ps -o rss= -p #{Process.pid}`.to_i/1024
+  memory_after = `ps -o rss= -p #{Process.pid}`.to_i
 
   puts({
     RUBY_VERSION => {
       gc: no_gc ? 'disabled' : 'enabled',
       time: time.round(2),
       gc_count: gc_stat_after[:count] - gc_stat_before[:count],
-      memory: "%d MB" % (memory_after - memory_before)
+      memory: "%d KB" % (memory_after - memory_before)
     }
   }.to_json)
 end
