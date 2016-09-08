860cb711ebdb2cca5989b394badb8aa9185d2b8b
Travis Spangle <travis.spangle@k2sports.com>
2016-09-08 08:20:32 -0700

    ski down a mountain with bfs

---
+++ Travis/mountain.rb
@@ -0,0 +1,85 @@
+require 'pry'
+
+class SkiPath
+  def find_ski_path(mountain)
+    queue_path = start_nodes(mountain[0])
+    checked = []
+    parents = {}
+    loop do
+      node = queue_path.shift
+      checked << node
+      return false if node.nil?
+      if mountain[node[0]][node[1]] == "F"
+        parents["F"] = parents[node.join("-")]
+        return parents 
+      end
+
+      children = find_children(mountain, node)
+      children.each do |child|
+        next if checked.include? child
+        parents[child.join("-")] = node.join("-")
+        queue_path << child
+      end
+    end
+  end
+
+  def start_nodes(row)
+    start_nodes = []
+    row.each_with_index do | element, index|
+      start_nodes << [0, index] if element.eql? 0
+    end
+    start_nodes
+  end
+
+  def find_children(mountain, node)
+    begin
+      children = []
+      children << [node[0]-1, node[1]] if node[0] > 0 && mountain[node[0]-1][node[1]] != 1 
+      children << [node[0], node[1]+1] if node[1] < mountain[node[0]].length && mountain[node[0]][node[1]+1] != 1 
+      children << [node[0]+1, node[1]] if node[0]+1 < mountain.length && mountain[node[0]+1][node[1]] != 1 
+      children << [node[0], node[1]-1] if node[1]-1 >= 0 && mountain[node[0]][node[1]-1] != 1 
+    rescue
+      binding.pry
+    end
+
+    children
+  end
+end
+
+mountain = Array.new(6)
+mountain[0] = [1,0,1,1,0,1]
+mountain[1] = [0,0,0,1,0,0]
+mountain[2] = [0,1,1,1,1,0]
+mountain[3] = [0,0,0,1,1,0]
+mountain[4] = [1,1,0,1,0,0]
+mountain[5] = [1,1,0,1,'F',1]
+
+puts "Mountain______________"
+mountain.each do |row|
+  puts row.inspect
+end
+
+ski_path = SkiPath.new
+path = ski_path.find_ski_path(mountain)
+node = path["F"]
+trail = []
+while node
+  trail << node
+  node = path[ node ]
+end
+
+trail = trail.reverse
+
+trail.each do |node|
+  begin
+    row,col = node.split("-")
+    mountain[row.to_i][col.to_i] = 2
+  rescue
+    binding.pry
+  end
+end
+
+puts "Mountain_Solved_________"
+mountain.each do |row|
+  puts row.inspect
+end
