3de06f719ff2813b0991d1b33b8f82a7540f6619
Travis Spangle <travis.spangle@k2sports.com>
2016-10-25 07:38:38 -0700

    fixed costs table

--- Travis/Ch7_Dijkstra.rb
+++ Travis/Ch7_Dijkstra.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 graph           = {}
 graph["book"]   = {"lp"=>5, "poster"=>0}
 graph["lp"]     = {"guitar" => 15, "drums" => 20}
@@ -6,7 +8,7 @@ graph["drums"]  = {"piano" => 10}
 graph["guitar"] = {"piano" => 20}
 graph["piano"]  = {}
 
-costs           = Hash.new(Float::INFINITY)
+costs           = Hash.new { |h,k| h[k] = Float::INFINITY }
 costs["lp"]     = 5
 costs["poster"] = 0
 
@@ -21,21 +23,9 @@ def find_lowest_cost_node(costs)
     if value < lowest_cost && !(@proccessed.include? node)
       lowest_cost = value
       lowest_cost_node = node
-    end
+    end 
   end
   lowest_cost_node
-end
-
-def find_lowest_cost_node(costs:, lowest_cost:Float::INFINITY, lowest_cost_node:nil)
-  return lowest_cost_node if costs.count.zero?
-
-  node, value = costs.shift
-  if value < lowest_cost && !(@proccessed.include? node)
-    lowest_cost = value
-    lowest_cost_node = node
-  end
-
-  find_lowest_cost_node(costs:costs, lowest_cost:lowest_cost, lowest_cost_node: lowest_cost_node)
 end
 
 def print_path(parents)
@@ -50,20 +40,19 @@ def print_path(parents)
   puts path.reverse.join(" -> ")
 end
 
-node = find_lowest_cost_node(costs:costs)
+node = find_lowest_cost_node(costs)
 
 while node
   cost = costs[node]
-  neighbors = graph[node]
-  for n in neighbors.keys
-    new_cost = cost + neighbors[n]
-    if costs[n] > new_cost
-      costs[n] = new_cost
-      parents[n] = node
+  graph[node].each do |neighbor, neighbor_cost|
+    new_cost = cost + neighbor_cost
+    if costs[neighbor] > new_cost
+      costs[neighbor] = new_cost
+      parents[neighbor] = node
     end
   end
   @proccessed << node
-  node = find_lowest_cost_node(costs:costs)
+  node = find_lowest_cost_node(costs)
 end
 
 print_path(parents)
